114610d64c874af5a94abe9ffc51ff5ec84aa299
Betul <115552237+Betulakyz35@users.noreply.github.com>
2025-06-16 12:12:51 +0300

    ByteTracking.py

--- ByteTracking.py
+++ ByteTracking.py
@@ -7,13 +7,11 @@ import numpy as np
 from ultralytics import YOLO
 from collections import defaultdict
 
-# Video ve model
 video_path = r"C:\Users\Betul\Downloads\tokat.mp4"
 cap = cv2.VideoCapture(video_path)
 model_name = r"C:\Users\Betul\Downloads\runs\runs\detect\train11\weights\best.pt"
 model = YOLO(model_name)
 
-# Senin sınıf isimlerin (custom)
 custom_class_names = [
     'ignored', 'pedestrian', 'people', 'bicycle', 'car', 'van', 'truck', 'tricycle', 'awning-tricycle', 'bus',
     'motor', 'others','plane', 'ship', 'storage tank', 'baseball diamond', 'tennis court', 'basketball court',
@@ -21,10 +19,8 @@ custom_class_names = [
     'soccer ball field', 'swimming pool', 'container crane'
 ]
 
-# 'ignored' sadece gösterilecek, takip edilmeyecek
-ignored_class_id = 0  # 'ignored' indexi
+ignored_class_id = 0 
 
-# Takip geçmişi
 track_history = defaultdict(lambda: [])
 
 while True:
@@ -42,18 +38,15 @@ while True:
         cx = int((x1 + x2) / 2)
         cy = int((y1 + y2) / 2)
 
-        # Sınıf adını güvenli şekilde al
+     
         class_name = custom_class_names[class_id] if class_id < len(custom_class_names) else f"class_{class_id}"
         color = (255, 0, 0) if class_id == ignored_class_id else (0, 255, 0)
-
-        # Kutu çizimi
+        
         cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
 
-        # Sınıf adı (takipli ya da takipsiz)
         label = f"ID:{track_id} {class_name.upper()}" if class_id != ignored_class_id else class_name.upper()
         cv2.putText(frame, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-        # Eğer 'ignored' değilse takip et
         if class_id != ignored_class_id:
             track = track_history[track_id]
             track.append((cx, cy))
